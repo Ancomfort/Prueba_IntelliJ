262283f5f4b4cb09c95d4bd900fd01d962dd75f1
angel <angel26022000@gmail.com>
2023-05-03 22:25:16 +0200

    commit prueba

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Esto es una prueba");
+        System.out.printf("Esto es una prueba 2");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
 
